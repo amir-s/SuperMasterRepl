5ea357593528094261b969e21127ab2317a37349
Amir Saboury <amir.saboury@gmail.com>
2014-11-22 17:44:32 -0500

    adding ILibrary interface

--- Replicas/Replica-Amir/src/LibraryServer.java
+++ Replicas/Replica-Amir/src/LibraryServer.java
@@ -1,15 +1,38 @@
-import javax.jws.*;
+public interface ILibrary{
+	// 0 : OK
+	// 1 : Server failiure 
+	// 2 : Username exists 
+	public String registerUser(String instName, String firstName, String lastName, String emailAddress, String phoneNumber, String username, String password);
+	// output: 2@
 
-@WebService
-public interface LibraryServer{
-	@WebMethod
-	public boolean registerUser(String InstName, String firstName, String lastName, String emailAddress, String phoneNumber, String username, String password);
-	@WebMethod
-	public boolean reserveBook(String InstName, String username, String password, String bookName, String authorName);
-	@WebMethod
-	public void setDuration(String InstName, String adminUsername, String adminPassword, String username, String bookName, String authorName, int days);
-	@WebMethod
-	public String getNonRetuners(String InstName, String adminUsername, String adminPassword, int days);
-	@WebMethod
-	public boolean reserveInterLibrary(String InstName, String username, String password, String bookName, String authorName);
+
+	// 0 : OK
+	// 1 : Server faillure
+	// 2 : Autentciation failed
+	// 3 : Book does not exists or no copies left
+	public String reserveBook(String instName, String username, String password, String bookName, String authorName);
+	// output: 2@
+
+
+	// 0 : OK
+	// 1 : Server faillure
+	// 2 : Autentciation failed
+	// 3 : Username or Book doesnt exists, or user doesnt have this book
+	public String setDuration(String instName, String adminUsername, String adminPassword, String username, String bookName, String authorName, int days);
+	// output: 2@
+
+
+	// 0 : OK
+	// 1 : Server faillure
+	// 2 : Autentciation failed
+	// 3 : Book does not exists or no copies left
+	public String reserveInterLibrary(String instName, String username, String password, String bookName, String authorName);
+	// output: 2@
+
+
+	// 0 : OK
+	// 1 : Server faillure
+	// 2 : Autentciation failed
+	public String getNonRetuners(String instName, String adminUsername, String adminPassword, int days);
+	// output: 2@InsName,FirstName,LastName,PhoneNumber@InsName,FirstName,LastName,PhoneNumber@InsName,FirstName,LastName,PhoneNumber
 }
