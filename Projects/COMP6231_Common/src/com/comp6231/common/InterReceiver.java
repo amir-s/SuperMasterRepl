25836dd5b08e802c6fa6c56fc30d974916d00695
zhaozhe <zhaozheid@126.com>
2014-11-29 11:34:10 -0500

    Communication Utils
    Communication Utils

---
+++ Projects/COMP6231_Common/src/com/comp6231/common/InterReceiver.java
@@ -0,0 +1,92 @@
+package com.comp6231.common;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.HashMap;
+import java.util.Map;
+
+public class InterReceiver extends Thread {
+	
+	/*
+	 * Mark - Constructors
+	 */
+	
+	public InterReceiver() {
+		handlers = new HashMap<String, InterReceiverHandler>();
+	}
+	
+	/*
+	 * Mark - Context - Properties
+	 */
+	 
+	private int portNumber;
+	
+	/*
+	 * Mark - Context - Getters & Setters
+	 */
+
+	public int getPortNumber() {
+		return portNumber;
+	}
+
+	public void setPortNumber(int portNumber) {
+		this.portNumber = portNumber;
+	}
+	
+	/*
+	 * Mark - Handlers - Properties
+	 */
+	 
+
+	private Map<String, InterReceiverHandler> handlers;
+	
+	/*
+	 * Mark - Handlers - Methods
+	 */
+	 
+	public void addHandler(InterReceiverHandler handler, String messageType) {
+		handlers.put(messageType, handler);
+	}
+	
+	/*
+	 * Mark - Basic - Methods
+	 */
+	
+	public void run() {
+		DatagramSocket socket = null;
+		
+		try {
+			socket = new DatagramSocket(portNumber);
+			byte[] receiveBytes = new byte[1000];
+			
+			while(true){
+				
+				// get receive message
+				DatagramPacket receivePacket = new DatagramPacket(receiveBytes, receiveBytes.length);
+				socket.receive(receivePacket);
+				
+				Inter receiveMessage = new Inter();
+				receiveMessage.decode(receiveBytes);
+				
+				// build send back message
+				Inter sendMessage = new Inter();
+				sendMessage.setType(Inter.TYPE_RETURN);
+				
+				// handle
+				String messageType = receiveMessage.getType();
+				InterReceiverHandler handler = handlers.get(messageType);
+				handler.handle(receiveMessage, sendMessage);
+
+				// send back
+				byte[] sendBytes = sendMessage.encode();
+				DatagramPacket sendPacket = new DatagramPacket(sendBytes, sendBytes.length, receivePacket.getAddress(), receivePacket.getPort());
+				socket.send(sendPacket);
+			}
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage() + " catch something");
+		} finally {
+			if (socket != null) socket.close();
+		}
+	}
+}
---
+++ Projects/COMP6231_Common/src/com/comp6231/common/InterReceiverHandler.java
@@ -0,0 +1,5 @@
+package com.comp6231.common;
+
+public interface InterReceiverHandler {
+	public void handle(Inter receiveMessage, Inter sendMessage);
+}
