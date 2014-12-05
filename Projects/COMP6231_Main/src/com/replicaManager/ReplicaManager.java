28ca02eb453c5d69d72f89404dae5d40878cee09
Amir Saboury <amir.saboury@gmail.com>
2014-12-04 19:24:57 -0500

    RM

--- Projects/COMP6231_Main/src/com/replicaManager/ReplicaManager.java
+++ Projects/COMP6231_Main/src/com/replicaManager/ReplicaManager.java
@@ -3,43 +3,67 @@ package com.replicaManager;
 import java.io.File;
 import java.io.IOException;
 import java.lang.ProcessBuilder.Redirect;
+import java.util.Scanner;
 
 import com.heartbeat.HeartBeatCallBack;
 import com.heartbeat.HeartBeatListener;
 
 public class ReplicaManager {
-	static void start () throws IOException {
+	private String replica = null;
+	private Process process = null;
+	private int heartBeatPort = -1;
+	Process startProcess () throws IOException {
+		System.out.println("STARTING REPLICA " + replica);
 		ProcessBuilder pb = new ProcessBuilder();
-		String path = "./amir-server.jar";
+		String path = "./" + replica + ".jar";
 		pb.command("java", "-jar",  path);
 		pb.redirectError(Redirect.INHERIT);
 		pb.redirectOutput(Redirect.INHERIT);
-		pb.directory(new File("./replicas/amir/"));
-		pb.start();
-//		
+		pb.directory(new File("./replicas/" + replica + "/"));
+		process = pb.start();
+		return process;
 	}
-	public static void main(String[] args) throws Exception {
-		HeartBeatListener hbl = new HeartBeatListener(4021, new HeartBeatCallBack() {
+	Process restartProcess() throws Exception {
+		System.out.println("RESTARTING REPLICA " + replica);
+		
+		System.out.println("KILLING REPLICA " + replica);
+		if (process != null) process.destroy();
+		Thread.sleep(1000);
+		return startProcess();
+	}
+	
+	public ReplicaManager(final String replica, int heartBeatPort) throws Exception {
+		this.replica = replica;
+		this.heartBeatPort = heartBeatPort;
+		
+		HeartBeatListener hbl = new HeartBeatListener(heartBeatPort, new HeartBeatCallBack() {
 			
-			@Override
 			public void up() {
-				System.out.println("UP");
-				
+				System.out.println("REPLICA " + replica + " IS UP");
 			}
 			
-			@Override
 			public void down() {
-				System.out.println("DOWN");
-				System.out.println("Starting again ...");
+				System.out.println("REPLICA " + replica + " IS DOWN");
+				System.out.println("STARTING " + replica + " AGAIN");
 				try {
-					ReplicaManager.start();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
+					restartProcess();
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
-		ReplicaManager.start();
 		
+		startProcess();
+		
+	}
+	
+	public static void main(String[] args) throws Exception {
+		ReplicaManager rm = new ReplicaManager("amir", 4021);
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			if (sc.next().equals("r")) {
+				rm.restartProcess();
+			}
+		}
 	}
 }
